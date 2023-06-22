817226f2c5a6a3625f2c885d62acba37ab1ce6dc
Lautaro Ezequiel Sanabria <lautaro95ss@gmail.com>
2023-06-21 22:26:43 -0300

    input object

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ var app = express();
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
 var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+app.use(cors({ optionsSuccessStatus: 200 }));  // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
@@ -20,8 +20,17 @@ app.get("/", function (req, res) {
 
 
 // your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+let responseObject = {}
+app.get("/api/:input", (req, res) => {
+  let { input } = req.params
+  res.json(input)
+  console.log(typeof input);
+  // const { date } = req.params;
+  // res.send({ date });
+  // let fecha = Math.floor(new Date().getTime() / 1000.0);
+  // let json = JSON.stringify(fecha);
+  // res.json({ fecha });
+  // console.log(typeof date);
 });
 
 
